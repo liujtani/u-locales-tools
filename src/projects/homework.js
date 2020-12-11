92b27f66466741fddc1be915f89842b565ee9987
liujtani <poi.nico.poi@gmail.com>
2020-12-11 17:14:51 +0800

    fix(homework): description 字段修正

--- src/projects/homework.js
+++ src/projects/homework.js
@@ -1,10 +1,17 @@
+const Path = require('path')
 module.exports = {
   name: 'umooc_homework_front',
   groups: [
     {
       dst: '个人作业_zh:part(part)?.json',
       src: 'ulearning/src/common/lang/:locale:part(part)?.json',
-      desc: '个人作业zh_js'
+      desc: (src) => {
+        if (Path.basename(src, '.json').endsWith('part')) {
+          return '个人作业zh_part.json'
+        } else {
+          return '个人作业zh_js'
+        }
+      }
     }
   ]
 };
