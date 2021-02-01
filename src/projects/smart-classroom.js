7547e7b8f595dcfb058e9778bba098f4c7f8011e
liujtani <poi.nico.poi@gmail.com>
2021-02-01 11:02:28 +0800

    fix: smart-classroom 配置字段修正

--- src/projects/smart-classroom.js
+++ src/projects/smart-classroom.js
@@ -3,8 +3,8 @@ module.exports = {
   groups: [
     {
       name: 'main',
-      src: 'smartClass.json',
-      dst: 'src/locales/:locale.json',
+      dst: 'smartClass.json',
+      src: 'src/locales/:locale.json',
       desc: 'smart-classroom 项目'
     }
   ]
